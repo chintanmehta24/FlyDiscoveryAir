f54301c574e32f3574d72dfca12671a22cc44b92
chintan-mehta24 <chintan.mehta24@gmail.com>
2014-12-06 20:59:27 +0530

    home page

---
+++ app/view/Home.js
@@ -0,0 +1,60 @@
+Ext.define('FlyDiscoveryAir.view.Home', {
+    extend: 'Ext.Panel',
+    xtype: 'home',
+    config: {
+    	layout: {
+    		type: "vbox",
+    		align: "center",
+    		pack: "center"
+		},
+		scrollable: "vertical",
+		cls: "home-view-cls",
+    	items: [{
+    		xtype: "fieldset",
+    		cls: "homeBtnGroup",
+    		maxWidth: 480,
+    		width: "100%",
+    		items:[{
+	    		xtype: "image",
+	    		cls: "airlineLogoCls",
+	    		width: "100%",
+				height: "auto",
+	    		mode: "element",
+	    		src: "resources/images/logo.png"
+	    	}, {
+	    		xtype: "button",
+	    		cls: "airlineMenuBtn",
+	    		text: "Book Flight",
+	    		iconAlign: "left",
+	    		iconCls: "icon-awesome  icon-plane",
+	    		ui: "plain"
+	    	}, {
+	    		xtype: "button",
+	    		cls: "airlineMenuBtn",
+	    		text: "My Flights",
+	    		iconAlign: "left",
+	    		iconCls: "icon-awesome  icon-user",
+	    		ui: "plain"
+	    	}, {
+	    		xtype: "button",
+	    		cls: "airlineMenuBtn",
+	    		text: "Check In",
+	    		iconAlign: "left",
+	    		iconCls: "icon-awesome icon-signin",
+	    		ui: "plain"
+	    	}, {
+	    		xtype: "button",
+	    		cls: "airlineMenuBtn",
+	    		text: "Boarding Pass",
+	    		iconAlign: "left",
+	    		iconCls: "icon-awesome  icon-ticket",
+	    		ui: "plain"
+	    	}, {
+	    		xtype: "image",
+	    		cls: "airlineBottomLogoCls",
+	    		mode: "element",
+	    		src: "resources/images/nigeria.png"
+	    	}]
+    	}]
+    }
+});
